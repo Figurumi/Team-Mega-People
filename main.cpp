5bceb9dbb7365141cdddc1b0bedcbd3de2e0b893
Figurumi <72786494+Figurumi@users.noreply.github.com>
2020-12-23 00:47:09 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include"hello_KAR.cpp"
 #include"hello_PDD.cpp"
+#include"hello_AVA.cpp"
 using namespace std;
 void main()
 {
   print_KAR();
   print_PDD();
+  print_AVA();
 }
